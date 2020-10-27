c43e6ccf3a92d8f3f1e2cfe49b93698e484b25ce
dommend <jani@penttinen.fi>
2020-10-27 14:19:57 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'fanikioski-MageKey_CategoryListWidget',
+    'MageKey_CategoryListWidget',
     __DIR__
 );
